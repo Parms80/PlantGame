a05d3415b2eaa100c61f1b971aa8a95c177cf4ac
k3ch0ng <k3ch0ng@gmail.com>
2018-06-09 15:56:42 +0100

    initial merge
    initial merge

---
+++ Assets/Scripts/FlyObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	float speed = 3;
+
+	// Update is called once per frame
+	void Update () {
+
+		float x = speed * Time.deltaTime * Random.RandomRange(-speed, speed);
+		float y = speed * Time.deltaTime * Random.RandomRange(-speed, speed);
+
+		transform.Translate(x, y , 0);
+	}
+
+	void OnMouseDown()
+    {
+		Debug.Log("Fly Clicked " + this.name);
+
+		this.gameObject.SetActive(false);
+		Destroy(this);
+    }
+
+}
---
+++ Assets/Scripts/NeighbourPopup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighbourPopup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		iTween.MoveBy(this.gameObject, iTween.Hash("y", 4, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .01));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PopUp()
+	{
+		transform.Translate(0, 0, 0);
+	}
+
+	void OnMouseDown()
+    {
+        Debug.Log("Neighbour Clicked " + this.name);
+
+       // this.gameObject.SetActive(false);
+       // Destroy(this);
+    }
+
+}
