7da0bc5d36a60772e08815e05203625f23064b95
Perminder Suman <permindersuman@Perminders-MacBook-Pro.local>
2018-06-09 19:44:50 +0100

    Commit after merge. Added 3 brushing scenes with animations

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public GameObject sunnyBackground;
+	public float levelSeconds;
+	public Text timeOverText;
+	private Slider slider;
+	private SpriteRenderer backgroundImage;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider> ();
+		backgroundImage = sunnyBackground.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = Time.timeSinceLevelLoad / levelSeconds;
+		backgroundImage.color = new Color(backgroundImage.color.r, backgroundImage.color.g, backgroundImage.color.b, slider.value);
+
+		if (slider.value == 1) {
+			timeOverText.gameObject.SetActive(true);
+		}
+	}
+}
