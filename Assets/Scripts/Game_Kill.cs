522204fe0388d55ce402a1fcf4ab028d34335a41
k3ch0ng <k3ch0ng@gmail.com>
2018-06-10 12:25:24 +0100

    kill

---
+++ Assets/Scripts/Game_Kill.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_Kill : MonoBehaviour
+{
+
+    public GameObject goTimer;
+
+    private Text txtTimer;
+    private float timer = 0;
+    public int maxTime = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        txtTimer = goTimer.GetComponent<Text>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int t = System.Convert.ToInt32(timer % 60);
+
+        if (t <= maxTime)
+        {
+            txtTimer.text = "Time: " + (maxTime - t);
+        }
+        else
+        {
+            // gameOver
+        }
+    }
+}
