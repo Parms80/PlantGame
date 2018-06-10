c4d0061b8ed29b2a2853b2aac3588b2d1dd64326
k3ch0ng <k3ch0ng@gmail.com>
2018-06-10 13:49:34 +0100

    curStage

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour {
+    
+	public static int curStage = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
